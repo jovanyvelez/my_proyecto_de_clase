31cf4df2f137dec0931a9db2b3f3602807d70ad6
jovany <jovany.velez@gmail.com>
2024-09-23 14:00:48 -0500

    Esta es la primera copia de mi proyecto

---
+++ src/routes/+page.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+    import Texto from "./Texto.svelte";
+    let nombre = $state("Futuros profesionales de Colombia")
+    let mostrarTexto = $state(false);
+    let miH2: HTMLHeadingElement | null;
+</script>
+
+<h1>
+    Alumnos de 11
+</h1>
+
+<p>
+    Aquí se mostrarán los alumnos de 11.
+    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deserunt adipisci natus delectus minus in, nemo rerum exercitationem facilis magni esse tempore explicabo. Nostrum, consectetur reiciendis! Deleniti minus facere voluptatem laboriosam.
+</p>
+
+<h2 bind:this={miH2}>Atentamente: {nombre}</h2>
+
+<button onclick={()=>mostrarTexto = !mostrarTexto}>Mostrar componente texto</button>
+
+{#if mostrarTexto}
+    <Texto />
+{/if}
+
+<style>
+    h1 {
+        color: blue;
+        text-align: center;
+    }
+    p {
+        color: red;
+        margin: 10px 20px;
+    }
+</style>
---
+++ src/routes/Texto.svelte
@@ -0,0 +1,13 @@
+
+<p>
+    "Esta pagina es  una alegoria a nuestros profes y padres
+    a quien tanto queremos y agradecemos por todo lo que nos han enseñado"
+</p>
+
+<style>
+    p {
+        color: green;
+        margin: 10px 20px;
+        font-size: 1.5rem;
+    }
+</style>
